92ca29597f15cfd9bb5613f51677a6ccf3ccc0f9
olenadudka <helen.lady2311@gmail.com>
2022-09-28 21:20:20 -0400

    first java commit

---
+++ src/com/syntax/class13/repl105.java
@@ -0,0 +1,15 @@
+package com.syntax.class13;
+
+import java.util.Scanner;
+
+public class repl105 {
+    public static void main(String[] args) {
+
+
+        StringBuffer sbf=new StringBuffer("Hello Friends");
+
+
+   System.out.println(sbf.reverse());
+
+    }}
+
---
+++ src/com/syntax/class9/jkh.java
@@ -0,0 +1,20 @@
+package com.syntax.class9;
+
+public class jkh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k=3, tot=0;
+
+do{
+
+   tot=tot+k;
+
+   k++;
+
+} while(k<11);
+
+System.out.print(tot);}}
+	
+
+
